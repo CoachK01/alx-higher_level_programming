1b3bc1f01ab67e44c00ca16af68f4cf2e328a3dc
CoachK01 <olakunleapara@gmail.com>
2023-01-10 18:03:29 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,6 @@
-x_integer(matrix=[[]]):
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             print("{:d}".format(col), end=" " if col != row[-1] else "")
-        print()i
+        print()
