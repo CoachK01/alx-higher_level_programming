d2e53ef764a246f50c6641551c12d4ffa799c263
CoachK01 <olakunleapara@gmail.com>
2023-01-11 11:55:39 +0000

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,83 +1,68 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * is_palindrome - Checks if a singly linked list is a palindrome
-  * @head: The head of the singly linked list
-  *
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-  */
+ * is_palindrome - checks if a singly linked list is palindrome or not
+ * @head: address of head of the singly linked list
+ * Return: 1 if palindrome and 0 if not
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *start = NULL, *end = NULL;
-    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+	size_t len, i;
+	listint_t *node1, *node2;
 
-    if (head == NULL)
-        return (0);
-
-    if (*head == NULL)
-        return (1);
-    
-    start = *head;
-    len = listint_len(start);
-    len_cyc = len * 2;
-    len_list = len_cyc - 2;
-    end = *head;
-
-    for (; i < len_cyc; i = i + 2)
-    {
-        if (start[i].n != end[len_list].n)
-            return (0);
-
-        len_list = len_list - 2;
-    }
-
-    return (1);
+	if (!head || !(*head))
+		return (1);
+	len = listint_len(*head);
+	for (i = 0; i < (len / 2); i++)
+	{
+		node1 = get_nodeint_at_index(*head, i);
+		node2 = get_nodeint_at_index(*head, (len - 1 - i));
+		if (node1->n != node2->n)
+			return (0);
+	}
+	return (1);
 }
 
 /**
-  * get_nodeint_at_index - Gets a node from a linked list
-  * @head: The head of the linked list
-  * @index: The index to find in the linked list
-  *
-  * Return: The specific node of the linked list
-  */
+ *listint_len - counts the number of nodes in a singly linked list
+ *@h: the head of the list
+ *Return: the number of nodes
+ */
+size_t listint_len(listint_t *h)
+{
+	listint_t *tmp;
+	size_t count = 0;
+
+	tmp = h;
+	while (tmp)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
+
+/**
+ *get_nodeint_at_index - returns the nth node of a list
+ *@head: pointer to first node
+ *@index: the index of the returned node
+ *Return: pointer to the nth node
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current = head;
-	unsigned int iter_times = 0;
+	unsigned int i = 0;
+	listint_t *nth;
 
-	if (head)
+	nth = head;
+	while (i < index)
 	{
-		while (current != NULL)
-		{
-			if (iter_times == index)
-				return (current);
+		nth = nth->next;
+		i++;
 
-			current = current->next;
-			++iter_times;
-		}
+		if (!nth)
+			return (0);
+
 	}
 
-	return (NULL);
-}
-
-/**
-  * slistint_len - Counts the number of elements in a linked list
-  * @h: The linked list to count
-  *
-  * Return: Number of elements in the linked list
-  */
-size_t listint_len(const listint_t *h)
-{
-	int lenght = 0;
-
-	while (h != NULL)
-	{
-		++lenght;
-		h = h->next;
-	}
-
-	return (lenght);
+	return (nth);
 }
