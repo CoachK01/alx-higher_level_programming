c038c90d36e1ee1c3f8aed440cfb506fe3d30c6f
CoachK01 <olakunleapara@gmail.com>
2023-01-31 13:37:09 -0800

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,13 +6,13 @@ class Rectangle:
     """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-         """Initialize a new Rectangle.
-         Args:
+        """Initialize a new Rectangle.
+        Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
-        """
-        self.width = width
-        self.height = height
+         """
+         self.width = width
+         self.height = height
 
     @property
     def width(self):
@@ -22,10 +22,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+             raise TypeError("width must be an integer")
+         if value < 0:
+             raise ValueError("width must be >= 0")
+         self.__width = value
 
     @property
     def height(self):
@@ -41,11 +41,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
-        return (self.__width * self.__height)
+         """Return the area of the Rectangle."""
+         return (self.__width * self.__height)
 
-    def perimeter(self):
-        """Return the perimeter of the Rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+     def perimeter(self):
+         """Return the perimeter of the Rectangle."""
+         if self.__width == 0 or self.__height == 0:
+             return (0)
+         return ((self.__width * 2) + (self.__height * 2))
