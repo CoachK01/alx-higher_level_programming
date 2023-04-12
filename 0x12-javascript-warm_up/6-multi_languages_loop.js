9f15ee7937216f25fd4dcce4d405cc89b166d382
CoachK01 <olakunleapara@gmail.com>
2023-04-12 16:25:30 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-  console.log(line);
+const multi = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i;
+for (i = 0; i < 3; i++) {
+  console.log(multi[i]);
+}
