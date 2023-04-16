89da7aba6e6774bb9b617883f19e6ea0045ab27c
CoachK01 <olakunleapara@gmail.com>
2023-04-16 10:39:48 -0700

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-# Lists all states from a database
-
+"""
+Lists all states from the datahase hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
+"""
 import sys
 import MySQLdb
 
